81a0f28f67acf1e11f25c96eef7a3c668b4ed438
venom_play <idaniil535@gmail.com>
2025-01-31 12:07:57 +0300

    upd indicator

---
+++ presentation/src/main/java/com/example/sneakershop/ui/components/CustomLoadingIndicator.kt
@@ -0,0 +1,24 @@
+package com.example.sneakershop.ui.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.example.sneakershop.ui.theme.customAccentColor
+
+@Composable
+fun CustomLoadingIndicator(
+    modifier: Modifier = Modifier
+) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+            .fillMaxSize()
+    ) {
+        CircularProgressIndicator(
+            color = customAccentColor
+        )
+    }
+}
--- presentation/src/main/java/com/example/sneakershop/ui/main/popular/Popular.kt
+++ presentation/src/main/java/com/example/sneakershop/ui/main/popular/Popular.kt
@@ -1,22 +1,18 @@
 package com.example.sneakershop.ui.main.popular
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.sneakershop.R
+import com.example.sneakershop.ui.components.CustomLoadingIndicator
 import com.example.sneakershop.ui.components.main.ProductsGrid
 import com.example.sneakershop.ui.components.topbar.SneakersTopBar
-import com.example.sneakershop.ui.theme.customAccentColor
 import com.example.sneakershop.ui.theme.customBackgroundColor
 
 @Composable
@@ -37,26 +33,19 @@ fun Popular(
         }
     ) { contentPadding ->
         if(state.popularProducts.isEmpty())
-            Box(
-                contentAlignment = Alignment.Center,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(contentPadding),
-            ) {
-                CircularProgressIndicator(
-                    color = customAccentColor
-                )
-            }
+            CustomLoadingIndicator(
+                modifier = Modifier.padding(contentPadding)
+            )
         else
-        ProductsGrid(
-            productsList = state.popularProducts,
-            onCardClick = { /*TODO*/ },
-            onFavoriteIconClick = { /*TODO*/ },
-            onButtonClick = { /*TODO*/ },
-            modifier = Modifier
-                .background(customBackgroundColor)
-                .padding(contentPadding)
-                .padding(top = 20.dp)
-        )
+            ProductsGrid(
+                productsList = state.popularProducts,
+                onCardClick = { /*TODO*/ },
+                onFavoriteIconClick = { /*TODO*/ },
+                onButtonClick = { /*TODO*/ },
+                modifier = Modifier
+                    .background(customBackgroundColor)
+                    .padding(contentPadding)
+                    .padding(top = 20.dp)
+            )
     }
 }
